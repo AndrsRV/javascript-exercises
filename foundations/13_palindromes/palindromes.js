ef299ca682ebe772c6f08aeefe8ab65db008f390
Andres Vina <andresricardovina@gmail.com>
2026-04-20 10:56:34 -0400

    Finished the exercises for palindromes function.

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,5 +1,8 @@
-const palindromes = function () {
-
+const palindromes = function (word) {
+    // remove punctuation and spaces, and convert to lower case
+    const cleanedWord = word.replace(/[^a-zA-Z0-9]/g, '').toLowerCase();
+    // check if cleanedWord is the same as its reverse
+    return cleanedWord === cleanedWord.split('').reverse().join('');
 };
 
 // Do not edit below this line
